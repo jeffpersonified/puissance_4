314fa2598296500ea046a8aa32284b7cf80db10d
Apprentice <apprentice@devbootcamp.com>
2012-10-31 21:18:53 -0700

    Add more game class tests and methods.

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,13 @@
+require_relative './player'
+
 class Game
-  attr_reader :red_player, :blue_player
+  attr_reader :red_player, :blue_player, :board
 
   def initialize
-    @red_player = {:color => "red"}
-    @blue_player = {:color => "blue"}
+    @red_player = Player.new('red')
+    @blue_player = Player.new('blue')
+    @turn = 1
+    @board = Board.new
   end
 
   def insertion_target_column
@@ -11,8 +15,32 @@ class Game
   end
 
   def current_player
-    turn % 2 = 0 ? @red_player : @blue_player
+    @turn % 2 != 0 ? @red_player : @blue_player
   end
 
+  def winning_combo?
+    @board.winning_combo?
+  end
+
+  def is_full?
+    @board.is_full?
+  end
+
+  def place_piece(player_id, column)
+    @board.place_piece(player_id, column)
+  end
+
+end
+
+
+
+
+class Board
+end
+
+class ColumnFullError < StandardError
+  def message
+    "Sorry, the column is full"
+  end
 
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -1,22 +1,26 @@
-require 'SimpleCov'
+require 'simplecov'
 SimpleCov.start
 
 require_relative '../lib/player.rb'
 
 describe Player do
 
-  context "#initialize" do
-    it "creates a player with a color attribute" do
-      player = Player.new("red")
-      player.color.should eq "red"
+  let(:player) { Player.new('red') }
 
+  context "#initialize" do
+
+    it "creates a player with a color attribute" do
+      player.color.should eq "red"
     end
+
   end
 
   context "#pick" do
+
     it "gets a number between 1 and 7 from the human" do
-      player = Player.new("red")
       player.pick.should be_between(1,7)
     end
+
   end
+
 end
