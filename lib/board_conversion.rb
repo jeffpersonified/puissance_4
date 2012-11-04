c5cdd103db348a19f542234cfe2e9414e33b16a9
Apprentice <apprentice@devbootcamp.com>
2012-11-03 17:25:51 -0700

    Add board conversion module and specs

--- lib/board_conversion.rb
+++ lib/board_conversion.rb
@@ -1,11 +1,27 @@
 module BoardConversion
 
-  def board_to_string(board)
-    # convert a board into a string
+  def board_to_string(board_array)
+    str = '|'
+    board_array.transpose.reverse.each do |row|
+      row.each do |cell|
+        cell == "" ? str << "." : str << cell
+      end
+      str << '|'
+    end
+    str
   end
 
   def string_to_board(string)
-    # convert a string into an array
+    string.split('|')[1..-1].map do |row|
+      replace_dots(row.split(''))
+    end
+    .reverse.transpose
+  end
+
+  private
+
+  def replace_dots(array)
+    array.map { |elt| elt == "." ? "" : elt }
   end
 
 end
