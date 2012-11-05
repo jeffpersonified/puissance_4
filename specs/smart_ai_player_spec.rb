b4ae603ff0e61a0b567db39054efddf6195b643b
Guillaume Galuz <guillaume.galuz@gmail.com>
2012-11-04 18:29:12 -0800

    added some tests for smart player

--- specs/smart_ai_player_spec.rb
+++ specs/smart_ai_player_spec.rb
@@ -3,35 +3,49 @@ require_relative '../lib/board'
 
 
 describe SmartAIPlayer do
-
-  let(:smart_ai_player) { SmartAIPlayer.new }
+  let(:smart_ai_player) { SmartAIPlayer.new('X') }
+  let(:board) { Board.new }
 
   context "#initialize" do
     it "creates a player with a symbol attribute" do
-      smart_ai_player.symbol.should eq "anonymous"
+      smart_ai_player.symbol.should eq "X"
     end
   end
 
   context "#pick" do
     it "receives a board and returns an integer between 1 and 7 from the computer" do
-      smart_ai_player.pick('board').should be_between(1,7)
+      smart_ai_player.pick(board).should be_between(1,7)
     end
-
+    
     it "takes a winning vertical move when available" do
-      board = Board.new
-      board.place_piece(1,1)
-      board.place_piece(1,1)
-      board.place_piece(1,1)
+      3.times { board.place_piece('X',1) }
       smart_ai_player.pick(board).should eq 1
-
     end
 
     it "takes a winning horizontal move when available" do
-      board = Board.new
-      board.place_piece(1,1)
-      board.place_piece(1,2)
-      board.place_piece(1,3)
+      3.times { |i| board.place_piece('X',i+1) }
       smart_ai_player.pick(board).should eq 4
+    end
+    
+    it "takes a winning diagonal move when available" do
+      board.place_piece('X',2)
+      1.times { board.place_piece('O',3) }
+      board.place_piece('X',3)
+      2.times {board.place_piece('O',4)}
+      board.place_piece('X',4)
+      3.times {board.place_piece('O',5)}
+      smart_ai_player.pick(board).should eq 5
+    end
+    
+    it "takes a winning diagonal move when available" do
+      2.times { board.place_piece('O',1) }
+      board.place_piece('X',1)
+      2.times{ board.place_piece('O',2) }
+      board.place_piece('X',2)
+      1.times{ board.place_piece('O',3) }
+      board.place_piece('X',3)
+      board.place_piece('X',4)
+      smart_ai_player.pick(board).should eq 1
     end
   end
 
